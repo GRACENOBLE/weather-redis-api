86e935aeedb20820eb2b136be49ab49a26a17b14
GRACENOBLE <gracenoble72@gmail.com>
2024-11-25 12:51:35 +0300

    Successfully served weather data

--- api/get-weather.go
+++ api/get-weather.go
@@ -1,13 +1,74 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/weather-redis-api/internal/types"
+)
 
 func Gin() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using defaults.")
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/weather", func(c *gin.Context) {
+		lat := c.Query("lat") //"0.3976677749854413"
+		lon := c.Query("lon") //"32.6378629998115"
+
+		if lat == "" || lon == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "lat and lon are required"})
+			return
+		}
+		apiKey := os.Getenv("OPENWEATHER_API_KEY")
+		if apiKey == "" {
+			log.Fatal("OPENWEATHER_API_KEY is not set in .env")
+		}
+		fmt.Println(apiKey)
+
+		url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%s&lon=%s&appid=%s", lat, lon, apiKey)
+
+		fmt.Printf("\nurl: %v\n", url)
+
+		res, err := http.Get(url)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch weather"})
+			return
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusOK {
+			c.JSON(res.StatusCode, gin.H{"error": "Invalid request"})
+			return
+		}
+
+		var weather types.WeatherResponse
+
+		if bodyBytes, err := io.ReadAll(res.Body); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		} else {
+			err := json.Unmarshal(bodyBytes, &weather)
+			if err != nil {
+				log.Printf(err.Error())
+			}
+
+			c.JSON(200, weather)
+		}
+
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port) // listen and serve on port 0.0.0.0:8081 or 0.0.0.0:8080 by default
 }
--- internal/types/types.go
+++ internal/types/types.go
@@ -1,1 +1,96 @@
 package types
+
+type WeatherResponse struct {
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	Timezone       string  `json:"timezone"`
+	TimezoneOffset int     `json:"timezone_offset"`
+	Current        struct {
+		Dt         int     `json:"dt"`
+		Sunrise    int     `json:"sunrise"`
+		Sunset     int     `json:"sunset"`
+		Temp       float64 `json:"temp"`
+		FeelsLike  float64 `json:"feels_like"`
+		Pressure   int     `json:"pressure"`
+		Humidity   int     `json:"humidity"`
+		DewPoint   float64 `json:"dew_point"`
+		Uvi        float64 `json:"uvi"`
+		Clouds     int     `json:"clouds"`
+		Visibility int     `json:"visibility"`
+		WindSpeed  float64 `json:"wind_speed"`
+		WindDeg    int     `json:"wind_deg"`
+		WindGust   float64 `json:"wind_gust"`
+		Weather    []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Rain struct {
+			OneH float64 `json:"1h"`
+		} `json:"rain"`
+	} `json:"current"`
+	Hourly []struct {
+		Dt         int     `json:"dt"`
+		Temp       float64 `json:"temp"`
+		FeelsLike  float64 `json:"feels_like"`
+		Pressure   int     `json:"pressure"`
+		Humidity   int     `json:"humidity"`
+		DewPoint   float64 `json:"dew_point"`
+		Uvi        float64 `json:"uvi"`
+		Clouds     int     `json:"clouds"`
+		Visibility int     `json:"visibility"`
+		WindSpeed  float64 `json:"wind_speed"`
+		WindDeg    int     `json:"wind_deg"`
+		WindGust   float64 `json:"wind_gust"`
+		Weather    []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Pop  float64 `json:"pop"`
+		Rain struct {
+			OneH float64 `json:"1h"`
+		} `json:"rain,omitempty"`
+	} `json:"hourly"`
+	Daily []struct {
+		Dt        int     `json:"dt"`
+		Sunrise   int     `json:"sunrise"`
+		Sunset    int     `json:"sunset"`
+		Moonrise  int     `json:"moonrise"`
+		Moonset   int     `json:"moonset"`
+		MoonPhase float64 `json:"moon_phase"`
+		Summary   string  `json:"summary"`
+		Temp      struct {
+			Day   float64 `json:"day"`
+			Min   float64 `json:"min"`
+			Max   float64 `json:"max"`
+			Night float64 `json:"night"`
+			Eve   float64 `json:"eve"`
+			Morn  float64 `json:"morn"`
+		} `json:"temp"`
+		FeelsLike struct {
+			Day   float64 `json:"day"`
+			Night float64 `json:"night"`
+			Eve   float64 `json:"eve"`
+			Morn  float64 `json:"morn"`
+		} `json:"feels_like"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+		DewPoint  float64 `json:"dew_point"`
+		WindSpeed float64 `json:"wind_speed"`
+		WindDeg   int     `json:"wind_deg"`
+		WindGust  float64 `json:"wind_gust"`
+		Weather   []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds int     `json:"clouds"`
+		Pop    int     `json:"pop"`
+		Rain   float64 `json:"rain,omitempty"`
+		Uvi    float64 `json:"uvi"`
+	} `json:"daily"`
+}
